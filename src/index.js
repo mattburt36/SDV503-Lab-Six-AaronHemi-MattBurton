b202f51f9160d0b3f525738f072caf83b021ccfd
Gainare <aaron-hemi@live.nmit.ac.nz>
2021-04-13 11:01:29 +1200

    task 1 setup

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/* task 1
+​Task_1:  Create a function that returns the total of all odd numbers up to and including the given arugrment. 
+The argument will be given as an odd number when you invoke the function. 
+Ex:  addOddNumbers(5)  -> 9  // 1 + 3 + 5 = 9
+Ex: addOddnumbers(13) -> 49 // 1 + 3 + 5 + 7 + 9 + 11 + 13 = 49 */
+
+
+// const oddOrEven = (arr) => arr.reduce((a, b) => a + b, 0) % 2 === 0 ? "even" : "odd"
+
+// console.log (oddOrEven([1,1,21,21,21,21,21,21,]))
+
+function addOddNumbers(arr) {
+    ​let arr = [];
+    let i = 0;
+    for(i=0; i <= x; i++) {​
+        if(i % 2 !== 0) {​
+            array.push + i 
+        }​
+        else
+        {​
+        }​
+    }​
+    console.log(i)
+}
+
+// ddNumbers(5)  //-> 9  // 1 + 3 + 5 = 9
+// addOddNumbers(13) //-> 49 // 1 + 3 + 5 + 7 + 9 + 11 + 13 = 49
+
+
